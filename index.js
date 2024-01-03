69671268b16316c2a7160f42876ab75dcb7b1efa
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-01-03 16:47:58 -0300

    Project upload, bug fixes

--- index.js
+++ index.js
@@ -18,16 +18,32 @@ db.connect();
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 
 
-app.get("/", async(req,res)=>{
+app.get("/", async (req,res)=>{
     const data = await db.query("SELECT * FROM books")
     const books = data.rows
     res.render("index.ejs",{
-       data:books,
+       data:books
     })
+})
+app.post("/", async (req,res)=>{
+    let userSearched = req.body.search.trim()
+    try{
+        console.log("HERE");
+        const searched = await db.query(`SELECT * FROM books WHERE LOWER(bookname) LIKE '${userSearched.toLowerCase()}%'`)
+        const result = searched.rows
+        console.log(result);
+        res.render("index.ejs",{
+            data:result
+        })}
+    catch(err){
+        console.log(err);
+        res.redirect("/")
+    }
 })
 
 app.listen(port,()=>{
