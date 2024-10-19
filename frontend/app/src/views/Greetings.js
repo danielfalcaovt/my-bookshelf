69d3f51eee375b78e5271134b5b73e3be0d73acd
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-19 16:21:16 -0300

    feat: writing effect add to greetings title

--- frontend/app/src/views/Greetings.js
+++ frontend/app/src/views/Greetings.js
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function Greetings() {
+  const allText = "Bem vindo ao meu mundo literário.";
+  const [texto, setTexto] = useState("");
+  function writingText() {
+    for (const i in allText) {
+      setTimeout(() => {
+        setTexto((oldValue) => {
+          return oldValue += allText[i];
+        });
+      }, 75 * i);
+    }
+  }
+  useEffect(() => {
+    writingText();
+  }, []);
   return (
     <section id="greetings">
       <article>
-        <h1>Bem vindo ao Meu Mundo Literário.</h1>
+        <h1>{texto}</h1>
         <p>
           Aqui você vai encontrar todos os livros que eu já li junto da minha
           humilde opinião sobre os mesmos. Lembrando que não sou nenhum expert
