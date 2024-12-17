5a7377b9afef5f20ed073ac7c6adba394df634da
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-17 18:17:25 -0300

    fix: filtered books style and books description

--- frontend/app/src/data.js
+++ frontend/app/src/data.js
@@ -4,104 +4,104 @@ const bookData = [
     img: "cosmos.jpg",
     rating: 8,
     note: "Um excelente mergulho na imensidão do universo.",
-    coverColor: '#11151E'
+    coverColor: "#11151E",
   },
   {
     bookname: "Código Limpo",
     img: "codigo-limpo.jpg",
     rating: 7,
-    note: "Excepcional.",
+    note: "Robert C. Martin aborda excepcionalmente diversos conceitos em relação ao padrão clean code na programação.",
   },
   {
     bookname: "Arte & Alma",
     img: "arte-e-alma.png",
-    rating: 5,
-    note: "Um romance comum com drama.",
-    coverColor: '#030A85'
+    rating: 7,
+    note: "Romance que explora os altos e baixos da vida, a conexão entre almas perdidas e a cura emocional.",
+    coverColor: "#030A85",
   },
   {
     bookname: "Você ligou para o SAM",
     img: "voce-ligou.png",
     rating: 4,
     note: "Apresenta uma premissa interessante, mas o autor não soube explorá-la.",
-    coverColor: '#FBDDDC'
+    coverColor: "#FBDDDC",
   },
   {
     bookname: "Verity",
     img: "verity.jpg",
     rating: 10,
     note: "Um dos melhores thrillers que eu já li.",
-    coverColor: '#05352A'
+    coverColor: "#05352A",
   },
   {
     bookname: "Layla",
     img: "layla.jpg",
     rating: 9,
-    note: "Bom. Tendo em vista que, foi a primeira vez em que ela escreveu nesse gênero",
-    coverColor: '#2C281D'
+    note: "Excepcional thriller de terror traçado por Colleen Hoover, que aborda sobre temas mais complexos e desconcertantes sobre o amor, a obsessão e os limites do que somos capazes de fazer por aqueles que amamos.",
+    coverColor: "#2C281D",
   },
   {
     bookname: "Como Pensar Sobre as Grandes Ideias",
     img: "como-pensar.png",
     rating: 6,
     note: "Uma boa introdução à filosofia.",
-    coverColor: '#4CBDC9'
+    coverColor: "#4CBDC9",
   },
   {
     bookname: "As Coisas que Você só vê Quando Desacelera",
     img: "as-coisas.jpg",
     rating: 6,
     note: "Pode fazer uma grande diferença na vida de pessoas que tem a mente aberta.",
-    coverColor: '#026394'
+    coverColor: "#026394",
   },
   {
     bookname: "A Fábrica de Cretinos Digitais",
     img: "a-fabrica.png",
     rating: 8,
-    note: "Livro essencial para todos os pertencentes à geração Z.",
+    note: "Michel Desmurget aborda hábilmente as consequências do consumo excessivo de telas no nosso cérebro, e como isso afeta negativamente todos os aspectos da nossa vida.",
   },
   {
     bookname: "O Capote",
     img: "o-capote.png",
     rating: 7,
-    note: "Excelente livro, alem de um pequeno mergulho na literatura russa.",
-    coverColor: '#FFFCF1'
+    note: "Um pequeno e tocante mergulho na literatura russa, no qual, Gogól aborda sobre como as vezes algo que para muitos parece irrelevante, para outros pode significar uma vida.",
+    coverColor: "#FFFCF1",
   },
   {
     bookname: "A Morte de Ivan Ilitch",
     img: "a-morte.jpg",
     rating: 9,
-    note: "Uma reflexão hábilmente traçada por Tolstói, relacionado a importância que damos à coisas fúteis.",
-    coverColor: '#FFFFFF'
+    note: "Tolstói reflete nesta obra sobre a futilidade dos valores que nós atribuímos ao bem material, e quão substituível a pessoa mais insubstituível pode ser.",
+    coverColor: "#FFFFFF",
   },
   {
     bookname: "Fortaleza Digital",
     img: "fortaleza-digital.jpg",
     rating: 8,
-    note: "Ótimo thriller tecnológico de Dan Brown. Explora os limites da ganância do homem.",
-    coverColor: '#0C6429'
+    note: "Primeira obra de Dan Brown, explora temas de confiança e segurança no mundo digital, com uma trama cheia de suspense sobre criptografia e conspirações..",
+    coverColor: "#0C6429",
   },
   {
     bookname: "Nunca Estivemos Aqui",
     img: "nunca-estivemos-aqui.png",
     rating: 7,
     note: "Thriller de Andrea Bartz, que explora bem a confiança que depositamos nas pessoas.",
-    coverColor: '#1B0D01'
+    coverColor: "#1B0D01",
   },
   {
     bookname: "1984 - George Orwell",
     img: "1984.jpg",
     rating: 10,
     note: "Um romance distópico traçado por George Orwell, que reflete muito sobre a moldagem da mente humana, e sobre como, com o gatilho certo, e o uso de uma emoção certa, coletivamente, podemos até mesmo as massas.",
-    coverColor: '#B42020'
+    coverColor: "#B42020",
   },
   {
-    bookname: 'A Biblioteca da Meia-Noite',
+    bookname: "A Biblioteca da Meia-Noite",
     img: "midnight-bookshelf.jpeg",
     rating: 8,
-    note: "Matt Haig reflete hábilmente sobre a imagem ilusória, em relação a outras vidas, e outras possíveis escolhas que poderíamos ter feito.",
-    coverColor: '#192B2F'
-  }
+    note: "Best seller de Matt Haig reflete sobre a ilusão da vida ideal e como a busca incessante por ela muitas vezes é vazia, já que o que mais queremos pode estar bem diante de nós.",
+    coverColor: "#192B2F",
+  },
 ];
 
 export default bookData;
